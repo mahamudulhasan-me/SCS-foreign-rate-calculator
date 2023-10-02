f5ea050766f68502e0a5cfdaa99d3d1577c9002f
Mahamdul Hasan <mahamudulhasan.org@gmail.com>
2023-10-02 15:03:48 +0600

    add select input

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,41 @@
+import FormControl from "@mui/material/FormControl";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import Select from "@mui/material/Select";
 const HomePage = () => {
-  return <div>Home</div>;
+  return (
+    <div className="w-4/5 mx-auto text-center mt-10 text-gray-700">
+      <h2 className="text-3xl font-bold ">International Coverage</h2>
+      <p>
+        You can calculate the international rate of the Sundarban Courier
+        Service (Pvt.) Ltd. here
+      </p>
+      <FormControl sx={{ m: 1, minWidth: 120 }}>
+        <InputLabel id="demo-simple-select-helper-label">Country</InputLabel>
+        <Select label="Country">
+          <MenuItem value={10}>Ten</MenuItem>
+          <MenuItem value={20}>Twenty</MenuItem>
+          <MenuItem value={30}>Thirty</MenuItem>
+        </Select>
+      </FormControl>
+      <FormControl sx={{ m: 1, minWidth: 120 }}>
+        <InputLabel id="demo-simple-select-helper-label">Country</InputLabel>
+        <Select label="Country">
+          <MenuItem value={10}>Ten</MenuItem>
+          <MenuItem value={20}>Twenty</MenuItem>
+          <MenuItem value={30}>Thirty</MenuItem>
+        </Select>
+      </FormControl>
+      <FormControl sx={{ m: 1, minWidth: 120 }}>
+        <InputLabel id="demo-simple-select-helper-label">Country</InputLabel>
+        <Select label="Country">
+          <MenuItem value={10}>Ten</MenuItem>
+          <MenuItem value={20}>Twenty</MenuItem>
+          <MenuItem value={30}>Thirty</MenuItem>
+        </Select>
+      </FormControl>
+    </div>
+  );
 };
 
 export default HomePage;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,13 @@
 import scsLogo from "@/assets/scsLogo.jpg";
+
 import Image from "next/image";
 const Header = () => {
   return (
-    <div className="px-[10%] flex justify-between items-center">
-      <Image width={70} src={scsLogo} alt="SCS Logo" />
+    <div className="bg-white px-[10%] flex justify-between items-center shadow-[rgba(0,_0,_0,_0.24)_0px_3px_8px] py-2">
+      <Image width={50} height={20} src={scsLogo} alt="SCS Logo" />
+      <button className="bg-[#1976D2] px-4 py-2 rounded-md text-white">
+        Calculate
+      </button>
     </div>
   );
 };
