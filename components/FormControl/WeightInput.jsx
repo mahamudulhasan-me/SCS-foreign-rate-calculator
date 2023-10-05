f39a094587ab40272557ef8f09738a4e3103da5e
mahamudulhasan-me <mahamudulhasan.org@gmail.com>
2023-10-05 13:29:42 +0600

    solve some problem

--- components/FormControl/WeightInput.jsx
+++ components/FormControl/WeightInput.jsx
@@ -3,7 +3,12 @@ import { InputAdornment, TextField } from "@mui/material";
 const WeightInput = ({ setWeight }) => {
   const handleInputChange = (event) => {
     const weight = event.target.value;
-    setWeight(weight);
+    if (weight < 0) {
+      setWeight(weight);
+      return;
+    }
+    const numericWeightValue = weight.replace(/\D/g, ""); // Remove non-numeric characters
+    setWeight(numericWeightValue);
   };
   return (
     <TextField
@@ -11,6 +16,7 @@ const WeightInput = ({ setWeight }) => {
       name="weight"
       size="small"
       type="number"
+      className="weight-input"
       inputProps={{ inputMode: "numeric", pattern: "[0-9]*" }}
       onChange={(e) => handleInputChange(e)}
       InputProps={{
